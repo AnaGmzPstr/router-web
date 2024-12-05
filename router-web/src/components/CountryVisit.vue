76d1eab0d8c26537445a12daa03815a206fd2d15
AnaGmzPastr <ana.gomez.7e6@itb.cat>
2024-12-05 18:17:14 +0100

    commit extra

--- router-web/src/components/CountryVisit.vue
+++ router-web/src/components/CountryVisit.vue
@@ -2,24 +2,20 @@
 import data from '../assets/data.json'
 import { useRoute } from 'vue-router';
 
-
 const dataVisit = data;
 const pathImg = '/images/'
 
 const route = useRoute();
 const id = route.params.id - 1;
 
-
-
 </script>
 
 <template>
     <!-- <p>Id: {{ $route.params.index }}</p> -->
-    <div class="">
-
+    <div class="experience-details">
         <h3>{{ dataVisit.destinations[id].experiences[$route.params.index].name }}</h3>
         <img :src="pathImg + dataVisit.destinations[id].experiences[$route.params.index].image"
         :alt="dataVisit.destinations[id].experiences[$route.params.index].image" />
-        <p>Descripción: {{ dataVisit.destinations[id].experiences[$route.params.index].description }}</p>
+        <p>{{ dataVisit.destinations[id].experiences[$route.params.index].description }}</p>
     </div>
 </template>
