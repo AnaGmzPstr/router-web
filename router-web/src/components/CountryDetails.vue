9cf67538bcd9f7976c79bc3a8bb8337096a4db00
AnaGmzPastr <ana.gomez.7e6@itb.cat>
2024-12-10 17:07:44 +0100

    go back funciona

--- router-web/src/components/CountryDetails.vue
+++ router-web/src/components/CountryDetails.vue
@@ -2,6 +2,8 @@
 import { useRoute } from 'vue-router';
 import { RouterLink, RouterView } from 'vue-router'
 
+import GoBack from "@/components/Return.vue"
+
 import data from '../assets/data.json'
 import { ref } from 'vue';
 import { watch } from 'vue';
@@ -17,15 +19,20 @@ watch(
         id.value = newId - 1
     }
 );
+
+const scroll=() =>{
+    setTimeout(() => {
+
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+    }, 500)
+}
 </script>
 
 <template>
     <div>
-        <button>
-            <RouterLink to="/">Return home</RouterLink>
-        </button>
         <!-- <h1>{{ $route.params.id }}</h1> -->
         <h1>{{ countryData.destinations[id].name }}</h1>
+        <GoBack/>
         <div class="destination-details">
             <img :src="pathImg + countryData.destinations[id].image" :alt="countryData.destinations[id].name" />
             <p>{{ countryData.destinations[id].description }}</p>
@@ -35,18 +42,17 @@ watch(
             <h2>Top experiences in {{ countryData.destinations[id].name }}</h2>
             <div class="cards">
                 <div v-for="(visit, i) in countryData.destinations[id].experiences" :key="visit.name" class="card">
-                    <div >
-                        <RouterLink :to="`/countryDetails/${route.params.id}/countryVisit/${i}`">
-                            <img :src="pathImg + visit.image" :alt="visit.name" />
-                            {{ visit.name }}
-                        </RouterLink>
-                    </div>
+
+                    <RouterLink :to="`/countryDetails/${route.params.id}/countryVisit/${i}`" @click="scroll">
+                        <img :src="pathImg + visit.image" :alt="visit.name" />
+                        <span class="card__text">{{ visit.name }}</span>
+                    </RouterLink>
+
                 </div>
             </div>
         </div>
-        <RouterView/>
+        <RouterView />
     </div>
 </template>
 
-<style>
-</style>
+<style></style>
